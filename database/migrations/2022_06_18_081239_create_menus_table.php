5cdb6cba238c8e8298a294a258cf239964d00c1c
temsmark <temsmark@gmail.com>
2022-06-18 13:15:39 +0300

    adding migrations

--- database/migrations/2022_06_18_081239_create_menus_table.php
+++ database/migrations/2022_06_18_081239_create_menus_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enum\StatusEnum;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,7 +16,12 @@ return new class extends Migration
     {
         Schema::create('menus', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->string('status')->default(StatusEnum::INACTIVE);
+            $table->string('icon')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
--- database/migrations/2022_06_18_081307_create_articles_table.php
+++ database/migrations/2022_06_18_081307_create_articles_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enum\StatusEnum;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,7 +16,14 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('status')->default(StatusEnum::INACTIVE);
+            $table->string('summary')->nullable();
+            $table->text('content')->nullable();
+            $table->string('featured_image')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
