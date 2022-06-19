5e9b30fa943e1b2927793461a54ee57d4df417f8
temsmark <temsmark@gmail.com>
2022-06-19 14:29:35 +0300

    updating composer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->isLocal()) {
+        if ($this->app->environment() !== 'production') {
             $this->app->register(IdeHelperServiceProvider::class);
         }
     }
