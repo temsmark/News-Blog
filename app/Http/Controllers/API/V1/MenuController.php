9c18ce11d9a726ed7f039a53315cd6a7f65cef5b
temsmark <temsmark@gmail.com>
2022-07-08 21:15:44 +0300

    adding articles count

--- app/Http/Controllers/API/V1/MenuController.php
+++ app/Http/Controllers/API/V1/MenuController.php
@@ -4,14 +4,16 @@ namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
 use App\Models\Menu;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class MenuController extends Controller
 {
-    public function index()
+    public function index(): JsonResponse
     {
         $menu = Menu::latest()
-            ->get(['id', 'name', 'slug','icon']);
+            ->withCount('articles')
+            ->get(['id', 'name', 'slug','icon','articles_count']);
 
         return response()->json([
             'data' => $menu,
