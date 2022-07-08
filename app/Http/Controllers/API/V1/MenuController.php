e0e0ca644d5c2035c6229e8408d411c4794d71aa
temsmark <temsmark@gmail.com>
2022-07-08 21:26:18 +0300

    mapping menus

--- app/Http/Controllers/API/V1/MenuController.php
+++ app/Http/Controllers/API/V1/MenuController.php
@@ -13,7 +13,16 @@ class MenuController extends Controller
     {
         $menu = Menu::latest()
             ->withCount('articles')
-            ->get(['id', 'name', 'slug','icon','articles_count']);
+            ->get(['id', 'name', 'slug', 'icon', 'articles_count'])
+            ->map(function ($menu) {
+                return [
+                    'id' => $menu->id,
+                    'name' => $menu->name,
+                    'slug' => $menu->slug,
+                    'icon' => $menu->icon,
+                    'articles_count' => $menu->articles_count,
+                ];
+            });
 
         return response()->json([
             'data' => $menu,
