20a91e809375178e72bba713d614978e8f1e30d6
temsmark <temsmark@gmail.com>
2022-07-17 13:35:09 +0300

    moving folder up to domain

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-const path = require('path')
+
 export default defineConfig({
     resolve: {
         alias: {
-            'ziggy': '/vendor/tightenco/ziggy/src/js',
-            'ziggy-vue': '/vendor/tightenco/ziggy/src/js/vue',
+            // 'ziggy': '/vendor/tightenco/ziggy/src/js',
+            // 'ziggy-vue': '/vendor/tightenco/ziggy/src/js/vue',
+            'ziggy': '/app/Domain/tightenco/ziggy/src/js',
+            'ziggy-vue': '/app/Domain/tightenco/ziggy/src/js/vue',
         },
     },
     plugins: [
